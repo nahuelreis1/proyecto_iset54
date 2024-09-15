cd6beed3db117ba6dac778b3b946444eb86a3e2e
nahuelreis1 <nahueldagamareis@outlook.com>
2024-09-15 16:12:27 -0300

    modificacion inicio-carrusel-encuentranos

--- iset54/settings.py
+++ iset54/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = ["app1/static",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
